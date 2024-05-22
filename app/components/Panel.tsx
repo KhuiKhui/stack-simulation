0e9982365bc6a3554caef0942ac2b2bf478b0634
KhuiHihi <khoikhuat0807@gmail.com>
2024-05-22 10:54:27 +0700

    Button fixed

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -1,12 +1,36 @@
-export default function Panel() {
+export default function Panel(props: {
+  stackType: number;
+  setStackType: Function;
+}) {
+  function handleClick(e: any): void {
+    if (props.stackType != e.target.id) {
+      e.target.style.backgroundColor = "antiquewhite";
+      if (props.stackType != 0) {
+        document.getElementById(`${props.stackType}`)!.style.backgroundColor =
+          "white";
+      }
+
+      props.setStackType(e.target.id);
+    } else {
+      e.target.style.backgroundColor = "white";
+
+      props.setStackType(0);
+    }
+  }
   return (
-    <div className="flex flex-col items-center mt-[50px] w-[40%] h-[200px] bg-[#BFCACD] border-2 border-black">
+    <div className="flex flex-col items-center mt-[50px] w-[40%] h-[150px] bg-[#BFCACD] border-2 border-black">
       <b className="mt-[10px]">Stack Control Panel</b>
 
-      <div className="flex flex-row justify-center items-center mt-[80px]">
-        <button className="bt">Increasing Monotonic Stack</button>
-        <button className="bt">Decreasing Monotonic Stack</button>
-        <button className="bt">Queue</button>
+      <div className="flex flex-row justify-center items-center mt-[30px]">
+        <button className="bt" id="1" onClick={(e) => handleClick(e)}>
+          Increasing Monotonic Stack
+        </button>
+        <button className="bt" id="2" onClick={(e) => handleClick(e)}>
+          Decreasing Monotonic Stack
+        </button>
+        <button className="bt" id="3" onClick={(e) => handleClick(e)}>
+          Queue
+        </button>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Stack from "./components/Stack";
 import Panel from "./components/Panel";
+import { useState } from "react";
 
 export default function Home() {
   const arr: number[] = [9, 0, 12, 3, 4, 101];
+  const [stackType, setStackType] = useState(0);
   return (
     <div className="flex justify-center items-center flex-col">
       <Stack arr={arr}></Stack>
-      <Panel></Panel>
+      <Panel stackType={stackType} setStackType={setStackType}></Panel>
     </div>
   );
 }
